31a786caccc1dabdb6c24fddff232b9359d62908
iyke <otiikechukwu24@gmail.com>
2026-02-18 23:48:52 +0100

    working

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import productReducer from './slices/productSlice';
+import storeReducer from './slices/storeSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
+    store: storeReducer,
   },
 });
 
